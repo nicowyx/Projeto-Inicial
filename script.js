d3facd6cd46e2354eabc730256ca8034665f4ec3
nicowyx <nicowyx@gmail.com>
2024-03-15 09:04:57 -0300

    variável nome

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
+var nome = document.getElementById("nome");
 
 function alertar(){
-    alert("Você clicou no botão!!!");
+    alert(nome.value + " " +"clicou no botão!!!");
 }
 
