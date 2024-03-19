f6ced8ea83d61fc8e340ae5b39eac307fe0ddd2a
nicowyx <nicowyx@gmail.com>
2024-03-19 09:13:57 -0300

    formulário

--- script.js
+++ script.js
@@ -14,15 +14,40 @@ var cidade = document.getElementById("cidade");
 var estado = document.getElementById("estado");
 
 function alertar(){
-    //alert(nome.value + " " +"clicou no botão!!!");
-    saida.innerText = "Nome: " + " " + nome.value +
+
+    //buscar endereço pelo cep
+    const url = `https://viacep.com.br/ws/${cep.value}/json`;
+
+    fetch(url)
+     .then(resposta=>resposta.json())
+     .then(data => {
+      logradouro.value = data.logradouro;
+      bairro.value = data.bairro;
+      cidade.value = data.cidade;
+      estado.value = data.uf;
+
+      saida.innerText = "Nome: " + " " + nome.value +
           "\n Email: " + " " + email.value +
           "\n Telefone: " + " " + telefone.value +
           "\n CEP: " + " " + cep.value +
           "\n Logradouro: " + " " + logradouro.value +
           "\n Número: " + " " + numero.value +
           "\n Complemento: " + " " + complemento.value +
+          "\n Bairro: " + " " + bairro.value +
           "\n Cidade: " + " " + cidade.value +
           "\n Estado: " + " " + estado.value;
-}
+
+     })
+     .catch(error=>alert(error))
+
+
+    //alert(nome.value + " " +"clicou no botão!!!");
+    
+          
+
+           
+
+
+
+        }
 
